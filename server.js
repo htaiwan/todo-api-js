c66f446fd8146aee0c50bc320b53580e4e0f23fc
Chien-Tai Cheng <htaiwan@yahoo-inc.com>
2016-04-23 22:39:17 +0800

    Creating New Todos

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: '跟老婆一起吃午餐',
-	completed: false
-}, {
-	id: 2,
-	description: '去菜市場',
-	completed: false
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
@@ -36,6 +33,17 @@ app.get('/todos/:id', function(req, res) {
 	} else {
 		res.status(404).send();
 	}
+});
+
+// POST /todos
+app.post('/todos', function(req, res) {
+	var body = req.body;
+
+	body.id = todoNextId++;
+
+	todos.push(body);
+
+	res.json(body);
 });
 
 app.listen(PORT, function() {
