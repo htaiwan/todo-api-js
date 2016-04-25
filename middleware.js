42b4fc156a3cfbd4ee04f0e1d52e06924d88cf07
Chien-Tai Cheng <htaiwan@yahoo-inc.com>
2016-04-25 15:06:27 +0800

    add user logout

--- middleware.js
+++ middleware.js
@@ -1,13 +1,29 @@
-module.exports = function (db) {
+var cryptojs = require('crypto-js');
+
+module.exports = function(db) {
 
 	return {
-		requireAuthentication: function (req, res, next) {
-			var token = req.get('Auth');
+		requireAuthentication: function(req, res, next) {
+			var token = req.get('Auth') || '';
+			// find token in the token table
+			db.token.findOne({
+				where: {
+					tokenHash: cryptojs.MD5(token).toString()
+				}
+			}).then(function(tokenInstance) {
+				if (!tokenInstance) {
+					throw new Error();
+				}
 
-			db.user.findByToken(token).then(function (user) {
+				// token exist
+				req.token = tokenInstance;
+				// search the user from token
+				return db.user.findByToken(token);
+			}).then(function(user) {
+				// find the user
 				req.user = user;
 				next();
-			}, function () {
+			}).catch(function() {
 				res.status(401).send();
 			});
 		}
--- server.js
+++ server.js
@@ -156,17 +156,31 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
+		userInstance = user;
 
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();
-		}
-	}, function() {
+		return db.token.create({
+			token: token
+		});
+
+	}).then(function (tokenInstance) {
+		// 成功將token記錄在token table中
+		res.header('Auth',tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function() {
+		// authenticate失敗或token記錄失敗
 		res.status(401).send();
+	});
+});
+
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function(req, res) {
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}).catch(function () {
+		res.status(500).send();
 	});
 });
 
